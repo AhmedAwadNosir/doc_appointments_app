e14302d738f0937be4e6e1753ffdb95e1364ba19
AhmedNosier <ahmedawadnosier@gmail.com>
2026-08-06 18:36:38 +0300

    -fack push to add devlopment prach to origin in githup

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  //create fake push to add local development to origing in githup
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
